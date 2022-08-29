1e3dcb8b31923ba7f821e2122c9567658f12a77c
ViswanadhBolla <viswanadth1234@gmail.com>
2022-08-29 10:45:47 +0530

    Testing

--- ProjectTesting/UnitTest1.cs
+++ ProjectTesting/UnitTest1.cs
@@ -1,16 +1,32 @@
+using System.Data.SqlClient;
+using System.Reflection;
+using DataAccess;
 namespace ProjectTesting
 {
     public class Tests
     {
+        public static DataBase db;
+        public static SqlDataReader dr;
         [SetUp]
         public void Setup()
         {
+            db = new DataBase();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            dr = db.getUserDetails("user1001", "userpass@1001");
+
+            Assert.AreEqual(dr.HasRows.ToString(), true.ToString());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int i = db.addPatientDetails("john", "doe", "male", 10, new DateTime(2012, 1, 1, 8, 30, 52));
+            int expi = 1;
+            Assert.AreEqual(expi, i);
         }
     }
 }
